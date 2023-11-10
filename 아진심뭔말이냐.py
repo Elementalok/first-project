723a1d522634431a5a88c1a9c37d2341c058250c
Elementalok <150311851+Elementalok@users.noreply.github.com>
2023-11-10 17:14:26 +0900

    Add files via upload

---
+++ 아진심뭔말이냐.py
@@ -0,0 +1,11 @@
+text = input('문자열을 입력하세요')
+
+for i in range (len(text)) :
+    for j in range (i+1):
+        print (text[j], end='')
+    print()
+    
+for i in range (len(text)) :
+    for j in range (reverse i - 1)
+      print (text[j], end='')
+    print()
